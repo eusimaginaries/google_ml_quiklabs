e342fd09a1bf9ab1203d380ca1ce2c229a550174
Euston Lee <lee.yousheng@gmail.com>
2020-09-24 00:49:57 +0800

    stage2

--- model.py
+++ model.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+import tensorflow.estimator as tflearn
+import tensorflow.contrib.layers as tflayers
+import tensorflow.contrib.metrics as tfmetrics
+import numpy as np
 
 CSV_COLUMNS  = \
 ('ontime,dep_delay,taxiout,distance,avg_dep_delay,avg_arr_delay,' + \
@@ -9,6 +13,8 @@ DEFAULTS     = [[0.0],[0.0],[0.0],[0.0],[0.0],[0.0],\
 
 def read_dataset(filename, mode=tf.contrib.learn.ModeKeys.EVAL,
                  batch_size=512, num_training_epochs=10):
+    # the actual input function passed to TensorFlow
+    def _input_fn():
       # This is double indented to make a later edit simpler
       if mode == tf.contrib.learn.ModeKeys.TRAIN:
          num_epochs = num_training_epochs
@@ -27,3 +33,59 @@ def read_dataset(filename, mode=tf.contrib.learn.ModeKeys.EVAL,
       features = dict(zip(CSV_COLUMNS, columns))
       label = features.pop(LABEL_COLUMN)
       return features, label
+    # return input function callback.
+    return _input_fn
+
+def get_features():
+    # Using three basic inputs
+    real = {
+      colname : tflayers.real_valued_column(colname) \
+          for colname in \
+            ('dep_delay,taxiout,distance').split(',')
+    }
+    sparse = {}
+    return real, sparse
+
+def linear_model(output_dir):
+    real, sparse = get_features()
+    all = {}
+    all.update(real)
+    all.update(sparse)
+    estimator = tflearn.LinearClassifier(model_dir=output_dir, feature_columns=all.values())
+    return estimator
+
+def serving_input_fn():
+    real, sparse = get_features()
+
+    feature_placeholders = {
+      key : tf.placeholder(tf.float32, [None]) \
+        for key in real.keys()
+    }
+    feature_placeholders.update( {
+      key : tf.placeholder(tf.string, [None]) \
+        for key in sparse.keys()
+    } )
+
+    features = {
+      # tf.expand_dims will insert a dimension 1 into tensor shape
+      # This will make the input tensor a batch of 1
+      key: tf.expand_dims(tensor, -1)
+         for key, tensor in feature_placeholders.items()
+    }
+    return tf.estimator.export.ServingInputReceiver(
+      features,
+      feature_placeholders)
+
+def run_experiment(traindata,evaldata,output_dir):
+  train_input = read_dataset(traindata,\
+                 mode=tf.contrib.learn.ModeKeys.TRAIN)
+  # Don't shuffle evaluation data
+  eval_input = read_dataset(evaldata)
+  train_spec = tf.estimator.TrainSpec(train_input, max_steps=1000)
+  eval_spec  = tf.estimator.EvalSpec(eval_input)
+  run_config = tf.estimator.RunConfig()
+  run_config = run_config.replace(model_dir=output_dir)
+  print('model dir {}'.format(run_config.model_dir))
+  estimator = linear_model(output_dir)
+
+  tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
--- task.py
+++ task.py
@@ -9,13 +9,27 @@ if __name__ == '__main__':
       help='Training data file(s)',
       required=True
   )
+  parser.add_argument(
+      '--evaldata',
+      help='Training data can have wildcards',
+      required=True
+   )
+  parser.add_argument(
+      '--output_dir',
+      help='Output directory',
+      required=True
+   )
+  parser.add_argument(
+      '--job-dir',
+      help='required by gcloud',
+      default='./junk'
+   )
   # parse args
   args = parser.parse_args()
   arguments = args.__dict__
   traindata = arguments.pop('traindata')
+  evaldata =  arguments.pop('evaldata')
+  output_dir = arguments.pop('output_dir')
 
-# Call read_dataset from model.py
-feats, label = model.read_dataset(traindata)
-# Find the average of all the labels that were read in
-avg = tf.reduce_mean(label)
-print(avg)
+tf.logging.set_verbosity(tf.logging.INFO)
+model.run_experiment(traindata,evaldata,output_dir)
